a1d465de6f496e30fc81fa09db66b5a148d283ca
Anna P <annapet200313@yandex.ru>
2025-12-26 16:19:07 +0800

    feat:RSS-PZ-08 add assets structure for data, photos and audio

--- src/pages/Game/GameScreen.ts
+++ src/pages/Game/GameScreen.ts
@@ -2,10 +2,27 @@ import { CreatorElement } from '../../utils/element-creator';
 import styless from './gameScreen.module.css';
 
 export function gameScreenUi() {
-  const gameScreenContainer = CreatorElement.createElement('div');
-  const gameScreenHeader = CreatorElement.createElement('header');
-  const gameScreenMain = CreatorElement.createElement('main');
+  const gameScreenContainer = CreatorElement.createElement('div', {
+    classes: [styless.game],
+  });
+  const gameScreenHeader = CreatorElement.createElement('header', {
+    classes: [styless.game__header, styless.container],
+  });
+  const gameScreenMain = CreatorElement.createElement('main', {
+    classes: [styless.main, styless.container],
+  });
+  const gameField = CreatorElement.createElement('div', {
+    classes: [styless.game__field],
+  });
+  const gameArea = CreatorElement.createElement('div', {
+    classes: [styless.game__area],
+  });
+  const sentenceBuilder = CreatorElement.createElement('div', {
+    classes: [styless.game__sentence_builder],
+  });
 
+  gameField.append(gameArea);
+  gameScreenMain.append(gameField, sentenceBuilder);
   gameScreenContainer.append(gameScreenHeader, gameScreenMain);
   document.body.append(gameScreenContainer);
 }
